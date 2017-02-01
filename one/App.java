a671964d0a28b080ef834ecc2dfcad8c2ad718ec
Dillon Ward <Dillon Ward>
2017-02-01 21:04:57 +0000

    working examples of threads - 3 simple threads

---
+++ one/App.java
@@ -0,0 +1,31 @@
+package multithreading.one;
+
+class Runner extends Thread{
+
+	public void run() {
+		for(int i = 0; i < 10; i++){
+			
+			System.out.println("Number: " + i);
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		// Both are running concurrently
+		Runner runner = new Runner();
+		runner.start();
+		
+		Runner runner1 = new Runner();
+		runner1.start();
+	}
+}
